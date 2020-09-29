fe2b87da9efc47ae6ca3a65aa455a09783bd7db7
JeongSeongHun054 <gnsl1465@naver.com>
2020-09-29 10:42:49 +0900

    mapStateToProps

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 `yarn add redux`
 `yarn add react-redux`
 `yarn add react-router-dom`
+
+## 2020.09.29
--- src/routes.js/Home.js
+++ src/routes.js/Home.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
-  const onChange = (e) => {
+  function onChange(e) {
     setText(e.target.value);
-  };
-  const onSubmit = (e) => {
+  }
+  function onSubmit(e) {
     e.preventDefault();
     setText("");
-  };
+  }
   return (
     <>
       <h1>To Do</h1>
-      <form action="" onSubmit={onSubmit}>
+      <form onSubmit={onSubmit}>
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+// mapStateToProps? => Redux State로부터 Component에 props으로써 전달하는 메소드
+// mapStateToProps에서 return 된 값이 Component의 props로 들어간다.
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+// connect란? Component와 Store를 연결해주는 것.
+export default connect(mapStateToProps)(Home);
